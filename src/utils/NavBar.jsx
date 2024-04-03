17c5db63d69083b2795125e34f6366e4c7985368
roba <10408936+roba-adnew@users.noreply.github.com>
2024-04-03 14:39:37 -0400

    Added styling to make buttons look like tabs

--- src/utils/NavBar.jsx
+++ src/utils/NavBar.jsx
@@ -3,15 +3,34 @@ import { useLocation } from 'react-router-dom'
 function NavBar() {
     const location = useLocation();
 
+    const navBarStyle = {
+        "display": "flex",
+        "flexDirection": "row"
+
+    }
+    
+    const homeButtonStyle = {
+        "textAlign": "center",
+        "flexGrow": "1",
+        "border": "1px solid black"
+    }
+
+    const productsButtonStyle = {
+        "textAlign": "center",
+        "flexGrow": "1",
+        "border": "1px solid black"
+
+    }
+
     return (
-        <>
-            <button>
+        <div style={navBarStyle}>
+            <span style={homeButtonStyle}>
                 <a href="/">Home page</a>
-            </button>
-            <button>
+            </span>
+            <span style={productsButtonStyle}>
                 <a href="products">Products page</a>
-            </button>
-        </>
+            </span>
+        </div>
     )
 }
 
