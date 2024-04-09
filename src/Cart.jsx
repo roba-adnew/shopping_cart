0ff62cd1a8581b38ff0494ccc2e5da5553605b6e
roba <10408936+roba-adnew@users.noreply.github.com>
2024-04-09 14:10:36 -0400

    Added total cart value at the bottom of the cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -3,16 +3,25 @@ import { CartProductCard } from './utils/Components'
 import './Products.css'
 
 function Cart({cart, updateCart}) {
+    const itemTotals = cart.map(item => (
+        parseInt(item.quantity) * item.product.price.toFixed(2)
+    ));
+
+    const sumTotal = itemTotals.reduce((sumTotal, itemTotal) => (sumTotal + itemTotal), 0).toFixed(2)
+
+
     return (
         <div 
             id="cart"
-            className={cart.length > 0 && "show"}>
+            className={cart.length > 0 && "show"}
+        >
             {cart.map(item => 
             <CartProductCard 
                 key={item.product.id}
                 cartItemInfo={item} 
                 updateCart={updateCart}  
             />)}
+            <div id="total">Total: ${sumTotal}</div>
         </div>
     )
 }
